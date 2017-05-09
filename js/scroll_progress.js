14c1dfcb1fdc717709504c0b81b8df2326dbcf55
insomnolent <jbcccsun@gmail.com>
2017-05-08 18:40:06 -0700

    started navbar and made progress bar

---
+++ js/scroll_progress.js
@@ -0,0 +1,8 @@
+$(window).on('load', function(){
+  $(window).on('scroll', function() {
+    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
+    var totalScroll = (wintop/(docheight-winheight))*100;
+    $(".KW_progressBar").css("width",totalScroll+"%");
+  });
+
+});
