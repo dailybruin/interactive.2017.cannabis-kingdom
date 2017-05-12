e8ded948600426c6c26fe143c476e1f89dd60f72
Nathan Tsai <nwtsai@gmail.com>
2017-05-12 16:46:13 -0700

    no message

---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+var NUMBER_OF_SECTIONS = 3;
+var NUMBER_OF_DAYS = 3;
+
+function sectionPressed(index) {
+	for(var i = 1; i <= NUMBER_OF_SECTIONS; i++)
+	{
+		var SectionID = "#Section" + i.toString();
+		if(i == index)
+			$(SectionID).addClass("active");
+		else
+			$(SectionID).removeClass("active");
+	}
+}
+
+function dayPressed(index) {
+	console.log("Day Pressed");
+	for(var i = 1; i <= NUMBER_OF_DAYS; i++)
+	{
+		var dayID = "#Day" + i.toString();
+		if(i == index)
+			$(dayID).addClass("active");
+		else
+			$(dayID).removeClass("active");
+	}
+}
