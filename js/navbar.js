c1490b77444dca680da6395a2b21104f8e11732b
local <local@Video-Mac-Mini.local>
2017-05-21 22:55:23 -0700

    added mobile slideout for navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,29 @@
-var NUMBER_OF_SECTIONS = 3;
-var NUMBER_OF_DAYS = 3;
+// var NUMBER_OF_SECTIONS = 3;
+// var NUMBER_OF_DAYS = 3;
 
-function sectionPressed(index) {
-	for(var i = 1; i <= NUMBER_OF_SECTIONS; i++)
-	{
-		var SectionID = "#Section" + i.toString();
-		if(i == index)
-			$(SectionID).addClass("active");
-		else
-			$(SectionID).removeClass("active");
-	}
-}
+// function sectionPressed(index) {
+// 	for(var i = 1; i <= NUMBER_OF_SECTIONS; i++)
+// 	{
+// 		var SectionID = "#Section" + i.toString();
+// 		if(i == index)
+// 			$(SectionID).addClass("active");
+// 		else
+// 			$(SectionID).removeClass("active");
+// 	}
+// }
 
-function dayPressed(index) {
-	console.log("Day Pressed");
-	for(var i = 1; i <= NUMBER_OF_DAYS; i++)
-	{
-		var dayID = "#Day" + i.toString();
-		if(i == index)
-			$(dayID).addClass("active");
-		else
-			$(dayID).removeClass("active");
-	}
-}
+// function dayPressed(index) {
+// 	console.log("Day Pressed");
+// 	for(var i = 1; i <= NUMBER_OF_DAYS; i++)
+// 	{
+// 		var dayID = "#Day" + i.toString();
+// 		if(i == index)
+// 			$(dayID).addClass("active");
+// 		else
+// 			$(dayID).removeClass("active");
+// 	}
+// }
 
+$( document ).ready(function() {
+    $(".button-collapse").sideNav();
+});
