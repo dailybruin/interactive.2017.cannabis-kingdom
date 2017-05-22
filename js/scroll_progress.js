ac707f25649b92d0db7139939ee255eba6c8649e
Austin Lee <ucaustinlee@g.ucla.edu>
2017-05-21 22:39:56 -0700

    content in

--- js/scroll_progress.js
+++ js/scroll_progress.js
@@ -1,6 +1,6 @@
 $(window).on('load', function(){
   $(window).on('scroll', function() {
-    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
+    var wintop = $(window).scrollTop(), docheight = $('.totalcontent').height(), winheight = $(window).height();
     var totalScroll = (wintop/(docheight-winheight))*100;
     $(".KW_progressBar").css("width",totalScroll+"%");
   });
